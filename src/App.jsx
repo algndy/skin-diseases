7eb150243b0c5c0ed8ebe039cc423eb0b05e0ac5
Mohamed Kamal Abdelaziz Ahmed <mohamedkamal.info@gmail.com>
2024-03-13 22:51:09 +0200

    Add diseases slideshow

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import styles from "./App.module.css";
 import HomeSection from "./components/HomeSection";
+import DiseasesSection from "./components/DiseasesSection";
 function App() {
   return (
     <div className={styles.container}>
       <Header />
       <HomeSection />
+      <DiseasesSection />
     </div>
   );
 }
---
+++ src/components/DiseasesSection.jsx
@@ -0,0 +1,51 @@
+import styles from "./DiseasesSection.module.css";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+const DISEASES = [
+  {
+    name: "Melanocytic",
+    info: "Melanocytic lesions are common and can be either benign or malignant. They arise from melanocytes, which are pigment-producing cells.",
+  },
+  {
+    name: "Actinic keratosis",
+    info: "Actinic keratosis, also known as solar keratosis, is a precancerous skin condition caused by prolonged exposure to ultraviolet (UV) radiation from the sun.",
+  },
+  {
+    name: "Dermatofibroma",
+    info: "Dermatofibroma is a benign skin lesion that usually appears as a firm nodule or bump on the skin. It is composed of fibrous tissue and often occurs on the legs.",
+  },
+  {
+    name: "Vascular lesion",
+    info: "Vascular lesions are abnormalities of blood vessels in the skin. They can appear as birthmarks, hemangiomas, or other vascular growths.",
+  },
+];
+
+function DiseasesSection() {
+  return (
+    <section className={styles.diseasesSection}>
+      <h1>Discover Dermatology</h1>
+      <div>
+        <Slide
+          slidesToScroll={1}
+          slidesToShow={1}
+          indicators={true}
+          autoplay={false}
+        >
+          {DISEASES.map((disease, i) => (
+            <div className={styles.card} key={i}>
+              <div>
+                <img src="homeImg.png" alt="homeImg.png" />
+              </div>
+              <div>
+                <h2>{disease.name}</h2>
+                <p>{disease.info}</p>
+              </div>
+            </div>
+          ))}
+        </Slide>
+      </div>
+    </section>
+  );
+}
+
+export default DiseasesSection;
